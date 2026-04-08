983aad1fd8c653e19e454f236fbf46939e40db16
Natan <natanhenriqueramossilva@gmail.com>
2026-04-07 22:30:35 -0300

    feat: add open file action using file manager and document model

--- src/main/java/ide/java/ui/MainWindow.java
+++ src/main/java/ide/java/ui/MainWindow.java
@@ -1,22 +1,29 @@
 package ide.java.ui;
 
+import ide.java.editor.Document;
+import ide.java.filesystem.FileManager;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class MainWindow extends JFrame {
 
     private EditorPanel editorPanel;
     private ConsolePanel consolePanel;
+    private FileManager fileManager;
 
     public MainWindow(){
 
         setTitle("Mini IDE");
-        setSize(100, 700);
+        setSize(1000, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         editorPanel = new EditorPanel();
         consolePanel = new ConsolePanel();
+
+        fileManager = new FileManager();
 
         JSplitPane splitPane = new JSplitPane(
                 JSplitPane.VERTICAL_SPLIT,
@@ -28,6 +35,41 @@ public class MainWindow extends JFrame {
 
         add(splitPane, BorderLayout.CENTER);
 
+        createMenu();
+
         setVisible(true);
+    }
+
+    private void createMenu(){
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem openItem = new JMenuItem("Open");
+
+        openItem.addActionListener(e -> openFile());
+
+        fileMenu.add(openItem);
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    private void openFile(){
+        JFileChooser chooser = new JFileChooser();
+
+        int result = chooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION){
+            File file = chooser.getSelectedFile();
+
+            String content = fileManager.openFile(file);
+
+            Document document = new Document(file, content);
+
+            editorPanel.setDocument(document);
+
+            setTitle("Mini-IDE - " + file.getName());
+        }
     }
 }
