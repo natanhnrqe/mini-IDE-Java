9fb8edc7609068d499e2e0af05a7af9925f3035c
Natan <natanhenriqueramossilva@gmail.com>
2026-04-12 20:20:07 -0300

    feat: add file explorer with JTree

---
+++ src/main/java/ide/java/ui/FileExplorerPanel.java
@@ -0,0 +1,45 @@
+package ide.java.ui;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.*;
+import java.io.File;
+
+public class FileExplorerPanel extends JPanel {
+
+    private JTree jTree;
+    private DefaultTreeModel treeModel;
+
+    public FileExplorerPanel(File rootDirectory) {
+        setLayout(new BorderLayout());
+
+        DefaultMutableTreeNode rootNode = createNode(rootDirectory);
+
+        treeModel = new DefaultTreeModel(rootNode);
+        jTree = new JTree(treeModel);
+
+        JScrollPane scrollPane = new JScrollPane(jTree);
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private DefaultMutableTreeNode createNode(File file){
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(file);
+
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+
+            if (files != null){
+                for (File child : files){
+                    node.add(createNode(child));
+                }
+            }
+        }
+        return node;
+    }
+
+    public JTree getjTree() {
+        return jTree;
+    }
+}
